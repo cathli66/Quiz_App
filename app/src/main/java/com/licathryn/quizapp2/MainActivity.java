31fb9b230b9810f912a11cd6a1fae342b3fcb01e
Cathryn <lifamily4@hotmail.com>
2020-02-05 14:56:04 -0500

    Added MainActivity.java code

--- app/src/main/java/com/licathryn/quizapp2/MainActivity.java
+++ app/src/main/java/com/licathryn/quizapp2/MainActivity.java
@@ -3,12 +3,42 @@ package com.licathryn.quizapp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button submitButton;
+    EditText responseText;
+    TextView displayText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        responseText=findViewById(R.id.responseEditText);
+        displayText=findViewById(R.id.textBox);
+        submitButton=findViewById(R.id.clickButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Hello mom");
+                Log.i("testButton", "Hi dad! "+responseText.getText().toString());
+                displayText.setText(displayText.getText().toString() + " Your name is " + responseText.getText().toString());
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    if(responseText.getText().toString().equals("TJ")) {
+                        displayText.setText("TJ Rocks!");
+                        responseText.setText("");
+                        responseText.setHint("That's a good name.");
+                    }
+                }
+            }
+        });
     }
 }
